7d0dcc5f4a63cb273686a2f42ba513cf15ddd7d4
ameneklis <ameneklis@EDPAMM>
2018-01-26 16:07:27 +0200

    Add project files.

---
+++ Calculator.Tests/UT_Calculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculator.Tests
+{
+    [TestClass]
+    public class UT_Calculator
+    {
+        [TestMethod]
+        public void testInnerElementMethod1()
+        {
+            ///1st passing test
+            string input = "(1+2)";
+            //should return 1+2
+            SOtests.Solution methodTOtest = new SOtests.Solution();
+            var output = methodTOtest.InnerElement(input);
+            Assert.IsTrue(output == "1+2");
+        }
+
+
+        [TestMethod]
+        public void testInnerElementMethod2()
+        {
+            ///1st passing test
+            string input = "((1+2)+3)/5+(5+6)";
+            //should return 1+2
+            SOtests.Solution methodTOtest = new SOtests.Solution();
+            var output = methodTOtest.InnerElement(input);
+            Assert.IsTrue(output == "5+6");
+        }
+    }
+}
---
+++ SOtests/Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
+using System.Threading;
+using System.Net.Http;
+
+namespace SOtests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Solution();
+        }    
+    }
+
+
+    public class Solution
+    {
+        List<string> ValidChars = new List<string>()
+        {
+            "+","-", "*", "/"," ","(",")",
+            "0","1","2","3","4","5","6","7","8","9"          
+        };
+
+        Dictionary<string, int> Operators = new Dictionary<string, int>();
+       
+        public  Solution()
+        {
+            //although mult has the smae priority with div as well as add with sub, 
+            //we assign 4 different presendce values for matter of simplicity
+            Operators.Add("*", 4);
+            Operators.Add("/", 3);
+            Operators.Add("+", 2);
+            Operators.Add("-", 1);
+        }
+
+        public int Calculate(string s)
+        {
+            //TO DO 
+            return 0;
+        }
+
+        #region Basic Operations
+        private string Add(string a, string b)
+        {
+            ///a and b are evaluated to valid int
+            return (int.Parse(a) + int.Parse(b)).ToString();
+        }
+
+        private string Sub(string a, string b)
+        {
+            ///a and b are evaluated to valid int
+            return (int.Parse(a) - int.Parse(b)).ToString();
+        }
+
+        private string Mult(string a, string b)
+        {
+            ///a and b are evaluated to valid int
+            return (int.Parse(a) * int.Parse(b)).ToString();
+        }
+
+        private string Div(string a, string b)
+        {
+            ///a and b are evaluated to valid int
+            return (int.Parse(a) / int.Parse(b)).ToString();
+        }
+        #endregion
+
+        public void CalculatorParser(string input)
+        {
+            //resebmle mathematical process pf evaluating an expression 
+            ///1st we evaluate parenthesis
+        }
+
+
+        public string InnerElement(string input)
+        {
+            if (input.IndexOf('(')>-1) //it contains Left parenthesis
+            {
+                int startIndex = input.LastIndexOf('(');
+                int endIndex = input.Substring(startIndex).IndexOf(')');
+                return input.Substring(startIndex + 1, endIndex -1);
+            }
+            ///else no parenthesis contained
+            return input;
+        }
+
+        public string RemoveSpaces(string input)
+        {
+            string res = input.Replace(" ", "");
+            return res;
+        }
+
+        public bool IsValidInputBaSe(string input)
+        {
+            foreach (var s in input)
+            {
+                if (!ValidChars.Contains(s.ToString()))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        //public void RemoveSpaces(out string input)
+        //{
+        //    // input = input.Replace(" ", "");
+        //}
+        public void EvaluateUnitElement(string unitElement)
+        {
+            ///we define Unit Element as a string that has no left or right parenthesis, 
+            ///contains only 1 basic operator and it evaluates to valid int
+        }
+
+        public string CreateUnitElement(string noParenthesisElement)
+        {
+            int sumOfOperators = 0;
+            string res = noParenthesisElement;
+            foreach (var s in noParenthesisElement)
+            {
+                if (Operators.ContainsKey(s.ToString()))
+                {
+                    sumOfOperators++;
+                }
+            }
+
+            while(sumOfOperators>0)
+            {
+
+            }
+
+            return res;
+        }
+
+        
+       
+    }
+
+}
